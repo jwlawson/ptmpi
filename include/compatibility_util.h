d9c6aaaae1ce91f13b4987d01de6b6259c82fe19
John Lawson <john@jwlawson.co.uk>
2016-08-30 16:59:42 +0100

    Gets compatibility util working with vectorset

--- include/compatibility_util.h
+++ include/compatibility_util.h
@@ -19,28 +19,56 @@
 #define _PTMPI_COMPATIBILITY_UTIL_H_
 
 #include "slave.h"
+#include "ptope/angle_check.h"
 
 namespace ptmpi {
 class CompatibilityUtil {
-static const char U;
-static const char T;
-static const double one;
-static const double minus_one;
-static const arma::blas_int incx;
+	static const char N;
+	static const char T;
+	static const char U;
+	static const double one;
+	static const double minus_one;
+	static const arma::blas_int int_one;
+	typedef Slave::CompatibilitySet CompatibilitySet;
 public:
-void
-compute_inner_products(Slave::VecSet const& vecset, arma::mat & output,
-		const std::size_t real_dim);
+	void
+	compute_compatibilities(ptope::VectorSet const& vectors, CompatibilitySet& output);
 
 private:
-arma::mat eucl;
-arma::vec hyp;
+	/** Workspace for computing inner products. */
+	arma::podarray<double> m_inner_products;
+	/** Check for valid angles. */
+	ptope::AngleCheck m_angle_check;
+
+	/** Get the smallest size needed to hold all inner products of vectors. */
+	std::size_t
+	priv_min_product_size( ptope::VectorSet const& vectors );
+	/** Get the row, col pair for an rfp index. */
+	std::pair<std::size_t, std::size_t>
+	priv_rfp_index_to_ij( std::size_t const index , arma::uword const nrows );
+	/** Find the inner products of all vectors in the vector set. */
+	void
+	priv_products( ptope::VectorSet const& vectors);
+	/** Prepare to compute products.
+	 * Make sure the workspace is large enough etc.
+	 */
+	void
+	priv_products_prepare( ptope::VectorSet const& vectos );
+	/** Compute the inner products of the vectors. */
+	void
+	priv_products_compute( ptope::VectorSet const& vectors );
+	/** Use computed inner products to determine compatibilities. */
+	void
+	priv_compatibility_from_products( ptope::VectorSet const& vectors ,
+			CompatibilitySet& output );
 };
 #ifdef ARMA_USE_BLAS
 #ifndef ARMA_BLAS_CAPITALS
 	#define arma_dsyr dsyr
+	#define arma_dsfrk dsfrk
 #else
 	#define arma_dsyr DSYR
+	#define arma_dsfrk DSFRK
 #endif
 extern "C" {
 	void arma_fortran(arma_dsyr)(
@@ -51,6 +79,33 @@ extern "C" {
 			const arma::blas_int* incx,
 			double* A,
 			const arma::blas_int* lda);
+	/**
+	 * Level 3 BLAS like routine for C in RFP Format.
+	 * 
+	 * DSFRK performs one of the symmetric rank--k operations
+	 *
+	 *		C := alpha*A*A**T + beta*C,
+	 *
+	 * or
+	 *
+	 *		C := alpha*A**T*A + beta*C,
+	 *
+	 * where alpha and beta are real scalars, C is an n--by--n symmetric
+	 * matrix and A is an n--by--k matrix in the first case and a k--by--n
+	 * matrix in the second case.
+	 */
+	void arma_fortran(arma_dsfrk)(
+		const char * TRANSR,
+		const char * UPLO,
+		const char * TRANS,
+		const arma::blas_int * N,
+		const arma::blas_int * K,
+		const double * ALPHA,
+		const double * A,
+		const arma::blas_int * LDA,
+		const double * BETA,
+		double * C
+	);
 }
 #endif
 }
--- src/compatibility_util.cc
+++ src/compatibility_util.cc
@@ -17,39 +17,86 @@
 #include "compatibility_util.h"
 
 namespace ptmpi {
-const char CompatibilityUtil::U = 'U';
+const char CompatibilityUtil::N = 'N';
 const char CompatibilityUtil::T = 'T';
+const char CompatibilityUtil::U = 'U';
 const double CompatibilityUtil::one = 1.0;
 const double CompatibilityUtil::minus_one = -1.0;
-const arma::blas_int CompatibilityUtil::incx = 1;
+const arma::blas_int CompatibilityUtil::int_one = 1;
 void
-CompatibilityUtil::compute_inner_products(Slave::VecSet const& vecset,
-		arma::mat & output, const std::size_t real_dim) {
-	const std::size_t num_vecs = vecset.size();
-	//TODO Oversize the matrices, to prevent continuous resizing
-	if(eucl.n_rows < real_dim || eucl.n_cols < num_vecs) {
-		eucl.set_size(real_dim, num_vecs + 100);
+CompatibilityUtil::compute_compatibilities(ptope::VectorSet const& vectors,
+		Slave::CompatibilitySet& output) {
+
+	priv_products( vectors );
+	priv_compatibility_from_products( vectors , output );
+}
+std::size_t
+CompatibilityUtil::priv_min_product_size( ptope::VectorSet const& vectors ) {
+	std::size_t num = vectors.size();
+	return ( num * ( num + 1 ) ) / 2;
+}
+std::pair<std::size_t, std::size_t>
+CompatibilityUtil::priv_rfp_index_to_ij( std::size_t const index ,
+		arma::uword const nrows ) {
+	std::size_t rfp_ncols = ( nrows + 1 ) / 2;
+	std::size_t rfp_nrows = nrows + 1 - ( nrows % 2 );
+	std::size_t rfp_col = index / rfp_nrows;
+	std::size_t row_cutoff = nrows / 2 + 1 + rfp_col;
+	std::size_t rfp_row = index % rfp_nrows;
+	std::size_t row;
+	std::size_t col;
+	if( rfp_row < row_cutoff ) {
+		row = rfp_row;
+		col = rfp_col + rfp_ncols - ( nrows % 2 );
+	} else {
+		col = rfp_ncols - rfp_col - 1 - ( nrows % 2 );
+		row = rfp_nrows - rfp_row - 1;
 	}
-	if(hyp.n_rows < num_vecs) {
-		hyp.set_size(num_vecs + 100);
+	return { row, col };
+}
+void
+CompatibilityUtil::priv_products( ptope::VectorSet const& vectors ) {
+	priv_products_prepare( vectors );
+	priv_products_compute( vectors );
+}
+void
+CompatibilityUtil::priv_products_prepare( ptope::VectorSet const& vectors ) {
+	m_inner_products.set_min_size( priv_min_product_size(vectors) );
+	m_inner_products.zeros();
+}
+void
+CompatibilityUtil::priv_products_compute( ptope::VectorSet const& vectors ) {
+	std::size_t total_dim = vectors.dimension();
+	std::size_t real_dim = total_dim - 1;
+	double const * vector_ptr = vectors.memptr();
+	double const * hyp_part_ptr = vector_ptr + real_dim;
+	arma::blas_int n = vectors.size();
+	arma::blas_int k = real_dim;
+	arma::blas_int lda = total_dim;
+	double * out_ptr = m_inner_products.memptr();
+
+	/* Compute hyperbolic part of product */
+	arma_fortran(arma_dsfrk)(&N, &U, &T, &n, &int_one, &one, hyp_part_ptr, &lda,
+			&one, out_ptr);
+	/* Compute inner product of euclidean part and subtract the hyperbolic
+	 * product. */
+	arma_fortran(arma_dsfrk)(&N, &U, &T, &n, &k, &one, vector_ptr, &lda,
+			&minus_one, out_ptr);
+}
+void
+CompatibilityUtil::priv_compatibility_from_products( ptope::VectorSet const& vectors ,
+		CompatibilitySet& comp ) {
+	std::size_t max_index = priv_min_product_size( vectors );
+	std::size_t num_vecs = vectors.size();
+	comp.resize(num_vecs);
+	for(auto & vec : comp) { vec.clear(); }
+	std::size_t row, col;
+	for(std::size_t index = 0; index < max_index; ++index) {
+		if( m_angle_check( m_inner_products[index] ) ) {
+			std::tie(row, col) = priv_rfp_index_to_ij( index , num_vecs );
+			comp[col].push_back( row );
+		}
 	}
-	if(output.n_rows < num_vecs || output.n_cols < num_vecs) {
-		output.set_size(num_vecs + 100, num_vecs + 100);
-	}
-	auto v_iter = vecset.begin();
-	for(std::size_t v_ind = 0; v_ind < num_vecs; ++v_ind, ++v_iter) {
-		arma::vec * vec = *v_iter;
-		eucl.col(v_ind) = vec->head_rows(real_dim);
-		hyp(v_ind) = vec->at(real_dim);
-	}
-	arma::blas_int nv = static_cast<arma::blas_int>(num_vecs);
-	arma::blas_int k = static_cast<arma::blas_int>(real_dim);
-	arma::blas_int lda = output.n_rows;
-	arma::blas_int ldeucl = eucl.n_rows;
-	double* outptr = output.memptr();
-	double* eucptr = eucl.memptr();
-	arma_fortran(arma_dsyr)(&U, &nv, &one, hyp.memptr(), &incx, outptr, &lda);
-	arma::arma_fortran(arma_dsyrk)(&U, &T, &nv, &k, &one, eucptr, &ldeucl, &minus_one, outptr, &lda);
 }
 }
 
